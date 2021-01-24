c06652958a487faff494327b867cdc006ffcdcf2
mikgn <kogan.miha@gmail.com>
2021-01-24 17:36:28 +0300

    fix 1.17 ruby solution

--- solutions/ruby/1_17.rb
+++ solutions/ruby/1_17.rb
@@ -8,18 +8,6 @@ def mult(a, b)
   end
 end
 
-# double(x) method doubles the number
-
-def double(x)
-  x + x
-end
-
-#  halve(x) method halves the number
-
-def halve(x)
-  x / 2
-end
-
 #  fast multiplication procedure using mult(a, b), double (x), halve (x) using recursion
 
 module FastMult
@@ -29,9 +17,9 @@ module FastMult
     elsif b == 1
       a
     elsif b.even?
-      0 + mult(a + a, b / 2)
+      0 + calc(a + a, b / 2)
     else
-      a + mult(a, b - 1)
+      a + calc(a, b - 1)
     end
   end
 end
